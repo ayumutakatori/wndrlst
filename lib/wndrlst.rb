0015c7fe762ddc8e94438257ce1e76f756656929
katsumata_ryo <ktmt.ryo@gmail.com>
2016-05-28 12:11:31 +0900

    generation_url test add

--- lib/wndrlst.rb
+++ lib/wndrlst.rb
@@ -7,6 +7,7 @@ module Wndrlst
   class API
 
     BASE_ENDPOINT = "https://a.wunderlist.com/api/v1"
+    attr_reader :params
 
     def initialize(client_id, access_token)
       @client_id     = client_id
@@ -18,52 +19,55 @@ module Wndrlst
     end
 
     def user
-      get(generate_url('user', @params))
+      get(generate_url('user', params))
     end
 
     def lists
-      get(generate_url('lists', @params))
+      get(generate_url('lists', params))
     end
 
     def list(id)
-      get(generate_url('lists', @params, id))
+      get(generate_url('lists', params, id))
     end
 
     def tasks(list_id, completed=false)
-      @params['list_id'] = list_id
-      @params['completed'] = completed
+      params = @params
+      params['list_id'] = list_id
+      params['completed'] = completed
 
-      get(generate_url('tasks', @params))
+      get(generate_url('tasks', params, params))
     end
 
     def task(id)
-      get(generate_url('tasks', @params, id))
+      get(generate_url('tasks', params, id))
     end
 
     def subtasks(task_id, completed=false)
-      @params['task_id'] = task_id
-      @params['completed'] = completed
+      params = @params
+      params['task_id'] = task_id
+      params['completed'] = completed
 
-      get(generate_url('subtasks', @params))
+      get(generate_url('subtasks', params))
     end
 
     def subtasks_from_list(list_id, completed=false)
-      @params['list_id'] = list_id
-      @params['completed'] = completed
+      params = @params
+      params['list_id'] = list_id
+      params['completed'] = completed
 
-      get(generate_url('subtasks', @params))
+      get(generate_url('subtasks', params))
     end
 
     private
 
     def generate_url(resource, params, target_id=nil)
-      params = @params.collect {|p, v| "#{p}=#{v}&"}.join("")
-      params.slice!(-1)
+      req_params = params.collect {|p, v| "#{p}=#{v}&"}.join("")
+      req_params.slice!(-1)
 
       if target_id
-        "#{BASE_ENDPOINT}/#{resource}/#{target_id}?#{params}"
+        "#{BASE_ENDPOINT}/#{resource}/#{target_id}?#{req_params}"
       else
-        "#{BASE_ENDPOINT}/#{resource}?#{params}"
+        "#{BASE_ENDPOINT}/#{resource}?#{req_params}"
       end
     end
 
